7be79c2c8e4dde1f39cb863c4c96edbcd6149f08
bane <amadoubane84@gmail.com>
2020-08-18 15:40:21 +0000

    Ajout de la page authentification

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    /**
+     * @Route("/", name="port")
+     */
+    public function indexHome()
+    {
+    	return $this->redirectToRoute('app_login');
+    }
+}
